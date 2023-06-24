ac6edc03ffa9730d7fe11c0243516a171de3176d
Noorazam8 <135502888+Noorazam8@users.noreply.github.com>
2023-06-24 07:01:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+alert("welcome to js land. \nHappy coding");
+
+var userName = "Muhammad";
+var myName = "Noor azam";
+console.log(myName)
+
+var message = "hello world";
+alert(message)
+
+// bio data of studunts
+var studentName = "harry";
+var address = "Islamabad";
+var rollNo =20005;
+var age = 15;
+// result of student
+alert("Name:" + studentName );
+alert("ADDRESS:" + address);
+alert("ROLLNO:" + rollNo);
+alert("AGE:" + age);
+//  variable alert
+alert("PIZZA \n PIZZ\n PIZ\n PI \n P");
+// alert of email
+
+var email = "noorazam@gmail.com"
+alert( "MY EMAIL ADDRESS:"+ email);
+var string ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(string);
+// visitor online clothing store
+var visitorName = "john doe";
+var productTitle = "t-shirt";
+var quantity = 5;
+// result
+
+var result = visitorName +"ordered"+  quantity + productTitle 
+console.log("result");
